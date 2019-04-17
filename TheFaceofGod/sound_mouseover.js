bed6aead7e6b770e0cc6be158126f64141cb7c2d
ImpendingTaco <impendingdoom.taco@gmail.com>
2019-04-16 22:22:53 -0600

    It got even dumber
    yeet

---
+++ TheFaceofGod/sound_mouseover.js
@@ -0,0 +1,10 @@
+function playAudio(sound) 
+{
+	var thissound=document.getElementById(sound);
+	thissound.play();
+}
+function pauseAudio(sound)
+{
+	var thissound=document.getElementById(sound);
+	thissound.pause();
+}
